e2f10b5886e6fd0352a1a01c72ac427fe6cd4750
andz-bb <andrew.thompson@budibase.com>
2024-03-14 14:03:56 +0000

    working upsert functionality

--- src/services/hubspot/update.ts
+++ src/services/hubspot/update.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { batchCreateAccounts } from "./create";
 
 const { HUBSPOT_ACCESS_TOKEN, HUBSPOT_OBJECT_ID, HUBSPOT_UNIQUE_PROPERTY } =
   process.env;
@@ -10,26 +11,30 @@ const config = {
   },
 };
 
-export async function batchUpdateAccounts(batch: any): Promise<any> {
-  console.log("batch: ", batch);
-
+export async function batchUpdateAccounts(batch: any) {
   const inputs = batch.map((x) => {
+    const allProperties = {
+      last_logged_in: x.LASTLOGGEDIN ? x.LASTLOGGEDIN.toJSON() : null,
+      total_apps: x.TOTALAPPS,
+      total_users: x.TOTALUSERS,
+      active_users: x.ACTIVEUSERS,
+      current_mrr: x.CURRENTMRR,
+      current_plan: x.CURRRENTPLAN,
+      subscription_status: x.SUBSCRIPTIONSTATUS,
+      billing_cycle: x.BILLINGCYCLE,
+      sso_configured: x.SSOCONFIGURED ? "true" : "false",
+      user_groups_count: x.USERGROUPSCOUNT,
+    };
+
+    // hubspot doesn't like when properties are null
+    const properties = Object.fromEntries(
+      Object.entries(allProperties).filter(([key, value]) => value !== null)
+    );
+
     return {
       id: x.TENANT,
       idProperty: HUBSPOT_UNIQUE_PROPERTY,
-      // hubspot api doesn't like these possibly being null on an update... come up with something smarter
-      properties: {
-        // last_logged_in: x.LASTLOGGEDIN ? x.LASTLOGGEDIN.toJSON() : null,
-        // total_apps: x.TOTALAPPS,
-        // total_users: x.TOTALUSERS,
-        // active_users: x.ACTIVEUSERS,
-        // current_mrr: x.CURRENTMRR,
-        // current_plan: x.CURRRENTPLAN,
-        // subscription_status: x.SUBSCRIPTIONSTATUS,
-        // billing_cycle: x.BILLINGCYCLE,
-        // sso_configured: x.SSOCONFIGURED ? "true" : "false",
-        user_groups_count: x.USERGROUPSCOUNT ?? 0,
-      },
+      properties,
     };
   });
 
@@ -37,19 +42,28 @@ export async function batchUpdateAccounts(batch: any): Promise<any> {
 
   try {
     const res = await axios.post(url, { inputs }, config);
-    console.log(res.data);
     return res.data;
   } catch (err) {
-    console.error("bad news:", err);
     const missingTenants =
       err.response?.data?.context?.missingObjectPropertyValueCoordinates ?? [];
+
     if (missingTenants.length > 0) {
       const ids = missingTenants.map((x) => x.split("tenant_id:")[1]);
-      console.table(ids);
-      console.log("blah");
-      // retry update without missing tenants
-      // get the missing tenant ids
-      // try to pull them from snowflake
+
+      const { tenantsToCreate, tenantsToUpdate } = batch.reduce(
+        (res, tenant) => {
+          if (ids.includes(tenant.TENANT)) {
+            res.tenantsToCreate.push(tenant);
+          } else {
+            res.tenantsToUpdate.push(tenant);
+          }
+          return res;
+        },
+        { tenantsToCreate: [], tenantsToUpdate: [] }
+      );
+
+      await batchCreateAccounts(tenantsToCreate);
+      await batchUpdateAccounts(tenantsToUpdate);
     } else throw err;
   }
 }
